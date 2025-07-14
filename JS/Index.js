b110aec916d9eb2d4b0ba09feec56d77b6430ef8
Jake <j.matthewbutcher@gmail.com>
2025-07-14 19:13:49 +0200

    Updated HTML and css

--- JS/Index.js
+++ JS/Index.js
@@ -1,5 +1,23 @@
+
+
 const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const hiddenElements = document.querySelectorAll('.hidden');
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+hiddenElements.forEach((el) => observer.observe(el));
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -9,5 +27,5 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
-})
+});
 
